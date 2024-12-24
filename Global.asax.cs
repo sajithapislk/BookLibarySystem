05cad73e6453798c2abb95f5ad0a19309a45b83f
sajithapislk <sajith@apis.lk>
2024-12-24 15:02:16 +0530

    add package and model with seeder

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using EBookStore.Data;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +18,9 @@ namespace BookLibarySystem
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Set the database initializer
+            Database.SetInitializer(new DatabaseSeeder());
         }
     }
 }
