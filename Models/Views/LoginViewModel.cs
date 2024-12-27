6c18aea845ca4992e010996d6a79b71459374c85
Saheerkhan98 <115021427+Saheerkhan98@users.noreply.github.com>
2024-12-28 00:49:57 +0530

    updates

--- Models/Views/LoginViewModel.cs
+++ Models/Views/LoginViewModel.cs
@@ -9,6 +9,7 @@ namespace BookLibarySystem.Models.Views
     public class LoginViewModel
     {
         [Required]
+        [Display(Name = "Email Address")]
         [EmailAddress]
         public string Email { get; set; }
 
@@ -17,4 +18,24 @@ namespace BookLibarySystem.Models.Views
         public string Password { get; set; }
     }
 
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Email Address")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
 }
