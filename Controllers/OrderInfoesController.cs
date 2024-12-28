a49694a572518b92290478ce133fb013c9d32ae2
sajithapislk <sajith@apis.lk>
2024-12-28 12:04:54 +0530

    view order info

---
+++ Controllers/OrderInfoesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BookLibarySystem.Models;
+
+namespace BookLibarySystem.Controllers
+{
+    public class OrderInfoesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+
+        // GET: OrderInfoes/Details/5
+        public ActionResult Index(int orderId)
+        {
+            var orderInfos = db.OrderInfos.Where(o => o.OrderId == orderId).ToList();
+
+            return View(orderInfos); // Example: passing the list to a view
+        }
+
+    }
+}
