3dea8e9662d1ed03854c25a39e939f100fb3a3c6
sajithapislk <sajith@apis.lk>
2024-12-29 19:17:24 +0530

    update

---
+++ Models/Views/AdminDashboardViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookLibarySystem.Models.Views
+{
+    public class AdminDashboardViewModel
+    {
+        public decimal TotalEarnings { get; set; }
+        public int TodayOrders { get; set; }
+        public int Customers { get; set; }
+        public int FeedbackCount { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+
+}
